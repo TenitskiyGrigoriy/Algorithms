21ae0aa5189c2d9ed33fcac3a54fc6d72e99f903
Grigory Tenitsky <uram17@mail.ru>
2022-08-17 21:41:37 +0300

    add SelectionSort

---
+++ src/SelectionSort.java
@@ -0,0 +1,40 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{64, 42, 73, 41, 31, 65, 15, 25, 67, 42, 74, 54, 36};
+
+        for (int step = 0; step < array.length; step++) {
+            System.out.println(arrayToString(array));
+            int index = min(array, step);
+
+            int tmp = array[step];
+            array[step] = array[index];
+            array[index] = tmp;
+        }
+    }
+
+    private static int min(int[] array, int start) {
+        int minIndex = start;
+        int minValue = array[start];
+        for (int i = start + 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
