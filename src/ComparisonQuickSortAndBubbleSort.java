2a1ccb1473ab7a3d8704bbdd9f79968d1d204c4d
Grigory Tenitsky <uram17@mail.ru>
2022-08-18 05:24:54 +0300

    add QuickSort and ComparisonQuickSortAndBubbleSort

---
+++ src/ComparisonQuickSortAndBubbleSort.java
@@ -0,0 +1,108 @@
+public class ComparisonQuickSortAndBubbleSort {
+    public static void main(String[] args) {
+
+        test1();
+        test2();
+    }
+
+    private static void quickSort(int[] arr, int from, int to) {
+
+        if (from < to) {
+
+            int divideIndex = partition(arr, from, to);
+
+            quickSort(arr, from, divideIndex - 1);
+
+            quickSort(arr, divideIndex, to);
+        }
+    }
+
+    private static int partition(int[] arr, int from, int to) {
+        int rightIndex = to;
+        int leftIndex = from;
+
+        int pivot = arr[from + (to - from) / 2];
+        while (leftIndex <= rightIndex) {
+
+            while (arr[leftIndex] < pivot) {
+                leftIndex++;
+            }
+
+            while (arr[rightIndex] > pivot) {
+                rightIndex--;
+            }
+
+            if (leftIndex <= rightIndex) {
+                swap(arr, rightIndex, leftIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
+    }
+
+    private static void bubbleSort(int[] arr) {
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[i - 1]) {
+                    swap(arr, i - 1, i);
+                    sorted = false;
+                }
+            }
+        }
+    }
+
+    private static void test1() {
+        int testLen = 10000;
+
+        int[] arr1 = new int[testLen];
+        int[] arr2 = new int[testLen];
+
+        System.out.println("\n-----Случайный массив-----");
+
+        for (int i = 0; i < testLen; i++) {
+            arr2[i] = arr1[i] = (int)Math.round(Math.random() * 10000);
+        }
+
+        System.out.println("Быстрая сортировка:");
+        measureTime(() -> quickSort(arr1, 0, testLen - 1));
+
+        System.out.println("Сортировка пузырьком:");
+        measureTime(() -> bubbleSort(arr2));
+    }
+
+    private static void test2() {
+        int testLen = 1000000;
+
+        int[] arr1 = new int[testLen];
+        int[] arr2 = new int[testLen];
+
+        System.out.println("\n-----Отсортированный массив-----");
+
+        for (int i = 0; i < testLen; i++) {
+            arr2[i] = arr1[i] = i;
+        }
+
+        System.out.println("Быстрая сортировка:");
+        measureTime(() -> quickSort(arr1, 0, testLen - 1));
+
+        System.out.println("Сортировка пузырьком:");
+        measureTime(() -> bubbleSort(arr2));
+    }
+
+    private static void measureTime(Runnable task) {
+        long startTime = System.currentTimeMillis();
+        task.run();
+        long elapsed = System.currentTimeMillis() - startTime;
+        System.out.println("Затраченное время: " + elapsed + " ms");
+    }
+}
+
---
+++ src/QuickSort.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+
+        int[] array = new int[] {64, 42, 73, 41, 31, 65, 15, 25, 67, 42, 74, 54, 36};
+
+        System.out.println(arrayToString(array));
+
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int from, int to) {
+
+        if (from < to) {
+
+            int divideIndex = partition(arr, from, to);
+
+            printSortStep(arr, from, to, divideIndex);
+
+            quickSort(arr, from, divideIndex - 1);
+
+            quickSort(arr, divideIndex, to);
+        }
+    }
+
+    private static int partition(int[] arr, int from, int to) {
+        int rightIndex = to;
+        int leftIndex = from;
+
+        int pivot = arr[from];
+        while (leftIndex <= rightIndex) {
+
+            while (arr[leftIndex] < pivot) {
+                leftIndex++;
+            }
+
+            while (arr[rightIndex] > pivot) {
+                rightIndex--;
+            }
+
+            if (leftIndex <= rightIndex) {
+                swap(arr, rightIndex, leftIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
+    }
+
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private static void printSortStep(int[] arr, int from, int to, int partitionIndex) {
+        System.out.print(arrayToString(arr));
+        System.out.print("\npartition at index: " + partitionIndex);
+        System.out.print(", left: " + arrayToString(Arrays.copyOfRange(arr, from, partitionIndex)));
+        System.out.println(", right: " + arrayToString(Arrays.copyOfRange(arr, partitionIndex, to + 1)) + "\n");
+    }
+}
