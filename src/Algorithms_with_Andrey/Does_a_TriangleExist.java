cc82a7f23fdc5bd43721a90b0300ddec05c28a3f
Grigory Tenitsky <uram17@mail.ru>
2022-11-29 12:09:48 +0400

    add Does_a_TriangleExist

---
+++ src/Algorithms_with_Andrey/Does_a_TriangleExist.java
@@ -0,0 +1,28 @@
+package Algorithms_with_Andrey;
+
+import java.util.Scanner;
+
+/**
+ * Input data
+ * Given three natural numbers a, b, c, written in separate lines. Determine if there is an unborn triangle with such sides.
+ *
+ * Output data
+ * If the triangle exists, print the line YES, otherwise print the line NO.
+ */
+
+public class Does_a_TriangleExist {
+    public static void main(String[] args) {
+
+        Scanner intScanner = new Scanner(System.in);
+        int a = intScanner.nextInt();
+        int b = intScanner.nextInt();
+        int c = intScanner.nextInt();
+
+        if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+}
