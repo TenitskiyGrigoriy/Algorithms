4eee461dc516eaa2c02173cc87baf89a2460d2cd
Grigory Tenitsky <uram17@mail.ru>
2023-03-12 22:04:32 +0400

    add FibonacciNumbers

--- src/Algorithms_with_Andrey/arrays/Array.java
+++ src/Algorithms_with_Andrey/arrays/Array.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
  * An array consisting of integers is given. Numbering of elements starts from 0. Write a program that outputs array elements whose numbers are even (0, 2, 4...).
  *
  * Input data
- * First, the number N is given — the number of elements in the array (1≤N≤100). Next, N numbers are written separated by a space — the elements of the array. The array consists of integers.
+ * First, the number N is given — the number of elements in the array (1≤N≤100). Next, N numbers are written separated by a space — the elements of the array.
+ * The array consists of integers.
  *
  * Output data
  * It is necessary to output all the elements of the array with even numbers.
